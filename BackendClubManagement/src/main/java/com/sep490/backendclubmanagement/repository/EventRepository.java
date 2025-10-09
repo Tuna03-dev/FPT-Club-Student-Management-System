a3d4ed0cdaf5c0f583c3ff61ef9daa74fdec7aae
Admin <duongdmhe170406@fpt.edu.vn>
2025-10-09 11:08:49 +0700

    Add 2 package "repository" and "service"

--- BackendClubManagement/src/main/java/com/sep490/backendclubmanagement/repository/EventRepository.java
+++ BackendClubManagement/src/main/java/com/sep490/backendclubmanagement/repository/EventRepository.java
@@ -1,4 +1,7 @@
 package com.sep490.backendclubmanagement.repository;
 
+import org.springframework.stereotype.Repository;
+
+@Repository
 public class EventRepository {
 }
--- BackendClubManagement/src/main/java/com/sep490/backendclubmanagement/service/EventService.java
+++ BackendClubManagement/src/main/java/com/sep490/backendclubmanagement/service/EventService.java
@@ -1,4 +1,7 @@
 package com.sep490.backendclubmanagement.service;
 
+import org.springframework.stereotype.Service;
+
+@Service
 public class EventService {
 }
