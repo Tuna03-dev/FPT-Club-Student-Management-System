ff86c36f565d1863c03d5d0b0d38b8f9dbe8c0a9
Haiyencute8903 <163019470+Haiyencute8903@users.noreply.github.com>
2025-10-15 08:01:32 +0700

    view post api

--- BackendClubManagement/src/main/java/com/sep490/backendclubmanagement/dto/response/LikeData.java
+++ BackendClubManagement/src/main/java/com/sep490/backendclubmanagement/dto/response/LikeData.java
@@ -4,7 +4,7 @@ import lombok.*;
 import java.time.LocalDateTime;
 
 @Getter @Setter @NoArgsConstructor @AllArgsConstructor @Builder
-public class LikeResponse {
+public class LikeData {
     private Long id;
     private Long userId;
     private String userName;
