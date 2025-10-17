d68cbc17d4a7a17007c7997508f180324b7ef47b
nguyenquoccuong <quoccuong2262k0@gmail.com>
2025-10-17 16:32:56 +0700

    feat(auth): push refresh token

--- BackendClubManagement/src/main/java/com/sep490/backendclubmanagement/exception/ErrorCode.java
+++ BackendClubManagement/src/main/java/com/sep490/backendclubmanagement/exception/ErrorCode.java
@@ -7,7 +7,7 @@ import org.springframework.http.HttpStatus;
 public enum ErrorCode {
     INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, 500, "Internal server error"),
     VALIDATION_ERROR(HttpStatus.BAD_REQUEST, 1000, "Validation failed"),
-    UNAUTHORIZED(HttpStatus.FORBIDDEN, 1010, "User is not allowed to access");
+    UNAUTHORIZED(HttpStatus.FORBIDDEN, 403, "User is not allowed to access");
 
     private final HttpStatus httpStatus;
     private final int code;
