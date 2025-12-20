0e48b93c9dbf92d12cebb8b2c098174d466538e2
Hoang Nh <hoangnguyenhuu11122003@gmail.com>
2025-12-21 00:19:48 +0700

    fix assign member when creation team

--- BackendClubManagement/src/main/java/com/sep490/backendclubmanagement/entity/RoleMemberShip.java
+++ BackendClubManagement/src/main/java/com/sep490/backendclubmanagement/entity/RoleMemberShip.java
@@ -6,8 +6,12 @@ import lombok.*;
 import java.time.LocalDate;
 
 @Entity
-@Table(name = "role_memberships", 
-       uniqueConstraints = @UniqueConstraint(columnNames = {"club_membership_id", "semester_id"}))
+@Table(
+        name = "role_memberships",
+        uniqueConstraints = @UniqueConstraint(
+                columnNames = {"club_membership_id", "semester_id", "clubrole_id"}
+        )
+)
 @Getter
 @Setter
 @NoArgsConstructor
@@ -24,18 +28,17 @@ public class RoleMemberShip extends BaseEntity {
     private ClubMemberShip clubMemberShip;
 
     @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "team_id")
-    private Team team;
-
-    @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "clubrole_id")
+    @JoinColumn(name = "clubrole_id", nullable = false)
     private ClubRole clubRole;
 
     @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "semester_id")
+    @JoinColumn(name = "semester_id", nullable = false)
     private Semester semester;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "team_id")
+    private Team team;
 
     @Column(name = "is_active", nullable = false)
     private Boolean isActive = true;
 }
-
