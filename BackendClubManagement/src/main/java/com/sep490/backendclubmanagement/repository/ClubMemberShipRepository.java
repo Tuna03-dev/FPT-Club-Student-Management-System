d2c29f0db06cfc0ff030146af1347c4f9328d424
Hoang Nh <hoangnguyenhuu11122003@gmail.com>
2025-12-22 22:55:37 +0700

    fix create assign member

--- BackendClubManagement/src/main/java/com/sep490/backendclubmanagement/repository/ClubMemberShipRepository.java
+++ BackendClubManagement/src/main/java/com/sep490/backendclubmanagement/repository/ClubMemberShipRepository.java
@@ -354,6 +354,7 @@ public interface ClubMemberShipRepository extends JpaRepository<ClubMemberShip, 
     SELECT DISTINCT u.id
     FROM club_memberships cm
     JOIN role_memberships rm ON rm.club_membership_id = cm.id
+    JOIN club_roles cr ON cr.id = rm.clubrole_id
     JOIN semesters s ON s.id = rm.semester_id
     JOIN users u ON u.id = cm.user_id
     WHERE cm.club_id = :clubId
@@ -362,6 +363,7 @@ public interface ClubMemberShipRepository extends JpaRepository<ClubMemberShip, 
       AND rm.is_active = 1
       AND rm.deleted_at IS NULL
       AND s.is_current = 1
+      AND cr.role_code NOT IN ('CLUB_PRESIDENT', 'CLUB_VICE_PRESIDENT')
 """, nativeQuery = true)
     List<Long> findAvailableMemberUserIds(@Param("clubId") Long clubId);
 
